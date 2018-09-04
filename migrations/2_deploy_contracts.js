ac2b8738c9b868a5012d754f0bcfcc67f3515096
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2018-09-04 21:52:03 +0300

    Remove Sandbox contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 var Dice = artifacts.require('./Dice.sol');
-var Sandbox = artifacts.require('./Sandbox.sol');
 
 module.exports = function(deployer) {
     deployer.deploy(Dice);
-    deployer.deploy(Sandbox);
 };
