b902e64ff3a3802ca3c05bf35b32b4461bc4b242
Dimitry Kislichenko <dimitrykislichenko@Dimitrys-iMac.local>
2018-11-26 13:22:22 +0200

    Add svg files for each dice option

--- src/common/components/__stories__/Dice.js
+++ src/common/components/__stories__/Dice.js
@@ -8,5 +8,29 @@ import Dice from '../Dice';
 
 storiesOf('common/Dice', module)
     .addDecorator(story => <CenteredContent story={story()} />)
-    .add('one', () => <Dice option="one" />)
-    .add('one large', () => <Dice option="one" size={200} />);
+    .add('all options', () => (
+        <div>
+            <Dice option="one" size={200} />
+            <Dice option="two" size={200} />
+            <Dice option="three" size={200} />
+            <Dice option="four" size={200} />
+            <Dice option="five" size={200} />
+            <Dice option="six" size={200} />
+        </div>
+    ))
+    .add('one', () => <Dice option="one" size={400} />)
+    .add('two', () => <Dice option="two" size={400} />)
+    .add('three', () => <Dice option="three" size={400} />)
+    .add('four', () => <Dice option="four" size={400} />)
+    .add('five', () => <Dice option="five" size={400} />)
+    .add('six', () => <Dice option="six" size={400} />)
+    .add('custom size', () => (
+        <div>
+            <Dice option="one" size={16} />
+            <Dice option="two" size={24} />
+            <Dice option="three" size={36} />
+            <Dice option="four" size={48} />
+            <Dice option="five" size={56} />
+            <Dice option="six" size={96} />
+        </div>
+    ));
--- src/common/index.js
+++ src/common/index.js
@@ -1,3 +1,5 @@
 export { default as AsyncValue } from './components/AsyncValue';
 export { default as HashAvatar } from './components/HashAvatar';
+export { default as Dice } from './components/Dice';
+
 export { default as RequireMetamask } from './containers/RequireMetamask';
