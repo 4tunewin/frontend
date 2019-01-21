528eb80c81c7f61cb75b3b1208eb5b66b866d9c2
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2019-01-21 11:38:51 +0200

    WIP

--- src/pages/dice/components/history/__stories__/GameHistory.js
+++ src/pages/dice/components/history/__stories__/GameHistory.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import styled from 'styled-components';
 
 import { Map } from 'immutable';
-import { ReduxProvider } from '../../../../../storybook';
+import { ReduxProvider, CenteredContent } from '../../../../../storybook';
 import GameHistory from '../GameHistory';
 import { history } from '../../__stories__/fixtures';
+
+const Wrapper = styled.div`
+    width: 500px;
+    height: 300px;
+`;
 
 storiesOf('dice/history/GameHistory', module)
     .addDecorator(story => (
@@ -16,6 +22,8 @@ storiesOf('dice/history/GameHistory', module)
             story={story()}
         />
     ))
+    .addDecorator(story => <Wrapper>{story()}</Wrapper>)
+    .addDecorator(story => <CenteredContent story={story()} />)
     .add('default', () => <GameHistory history={history} />)
     .add('loading', () => <GameHistory history={history} loading />)
     .add('empty', () => <GameHistory />);
--- src/sagas/dice.js
+++ src/sagas/dice.js
@@ -35,36 +35,51 @@ export function* placeBetAsync({ web3, type, payload }) {
 
     const diceInstance = yield call(DiceContract.instance);
 
+    console.info('Placing bet', {
+        commit,
+        commitLastBlock,
+        gasPrice,
+        signature: {
+            v: signature.v,
+            r: signature.r,
+            s: signature.s,
+        },
+    });
+
+    const event = yield call(
+        diceInstance.methods.placeBet,
+        setBitsForIndexes(payload.dices),
+        payload.modulo,
+        commitLastBlock,
+        commit,
+        signature.v,
+        signature.r,
+        signature.s,
+    );
+
+    const channel = eventChannel(emitter => {
+        event
+            .send({
+                from: web3.account,
+                value: web3.client.utils.toWei(
+                    toString(payload.amount),
+                    'ether',
+                ),
+                gas: 200000,
+                gasPrice,
+            })
+            .on('transactionHash', hash => emitter(hash))
+            .on('error', error => {
+                console.log(`Failed to place bet; ${error.message}`);
+                // WEB3 does not allow us to catch exception thrown by RPC because of
+                // implementation of web3 PromiEvent.
+                emitter(new Error(error));
+            });
+
+        return () => {};
+    });
+
     try {
-        console.info('Placing bet', { commit, commitLastBlock, gasPrice });
-
-        const event = yield call(
-            diceInstance.methods.placeBet,
-            setBitsForIndexes(payload.dices),
-            payload.modulo,
-            commitLastBlock,
-            commit,
-            signature.v,
-            signature.r,
-            signature.s,
-        );
-
-        const channel = eventChannel(emitter => {
-            event
-                .send({
-                    from: web3.account,
-                    value: web3.client.utils.toWei(
-                        toString(payload.amount),
-                        'ether',
-                    ),
-                    gasPrice,
-                })
-                .on('transactionHash', hash => emitter(hash))
-                .on('error', error => emitter(new Error(error)));
-
-            return () => {};
-        });
-
         const transactionHash = yield take(channel);
         yield put(placeBetSuccess(transactionHash));
     } catch (e) {
