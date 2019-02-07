e1cc6c4649ace13503c9dfab9066fd6df2598329
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2019-02-08 00:32:43 +0200

    Change garphql uri

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -4,7 +4,7 @@ export default {
         id: 1,
     },
     graphql: {
-        uri: 'graphql.4tune.win',
+        uri: 'graphql.4tune.win:8080',
     },
     // An URL of block explorer
     explorerUrl: 'https://www.etherscan.io',
